e530a94641ed135ed6f7d20a9c8dc4cc1499ab0e
Jacob Butler <jak@Jacobs-MBP.uncg.edu>
2019-09-11 16:09:00 -0400

    No significant changes. Starting on second drop list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -154,19 +154,13 @@ export class AppComponent {
 
 
     artists = [
-    'Artist I - Davido',
-    'Artist II - Wizkid',
-    'Artist III - Burna Boy',
-    'Artist IV - Kiss Daniel',
-    'Artist V - Mayorkun',
-    'Artist VI - Mr. Eazi',
-    'Artist VII - Tiwa Savage',
-    'Artist VIII - Blaqbonez',
-    'Artist IX - Banky W',
-    'Artist X - Yemi Alade',
-    'Artist XI - Perruzi',
-    'Artist XII - Seyi Shay',
-    'Artist XIII - Teni'
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G'
   ];
 
   }
