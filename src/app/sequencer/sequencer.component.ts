af83d268fec71362202adfda9e9cd436afcda68c
Shaun <sasheikh@uncg.edu>
2019-10-09 15:58:21 -0400

    think before you commit

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -22,18 +22,19 @@ export class SequencerComponent implements OnInit {
 
   }
 
-  //pass in 1. sampler value(C4) 2. point on transport(0:0:0)
+  //(not added)pass in 1. sampler value(C4) 2. point on transport(0:0:0)
+  //include in arguments toggleValue
   activate(cellId){
 
 
     //if(toggleValue = 0){
-    //toggleValue=1
+    //sequencerToggle[r][c]=1
     //turn on engaged color
     document.getElementById(cellId).style.backgroundColor = "yellow";
     //schedule value on transport}
     //else{
     //that means toggleValue=1 so it's engaged
-    //so toggleValue=0 now
+    //so sequencerToggle[r][c]=0 now
     //take the note out of the transport scheduler
     //change color back
     //document.getElementById(cellId).style.backgroundColor = "black";
