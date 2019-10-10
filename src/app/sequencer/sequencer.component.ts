a01e637c65db99c76e8416878dbad27bbb31e44e
Shaun <sasheikh@uncg.edu>
2019-10-09 21:51:38 -0400

    sequencer is a baby

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -21,7 +21,6 @@ export class SequencerComponent implements OnInit {
   ngOnInit() {
 
 
-
   }
 
   //(not added)pass in 1. sampler value(C4) 2. point on transport(0:0:0)
@@ -31,6 +30,7 @@ export class SequencerComponent implements OnInit {
     if(toggleValue == 0){
       document.getElementById(cellId).style.backgroundColor = "yellow";
       this.setSeqCellVal(cellId);
+      console.log(sequencerToggle);
       //schedule value on transport
     }
     else{
@@ -38,6 +38,7 @@ export class SequencerComponent implements OnInit {
       this.setSeqCellVal(cellId);
       //take the note out of the transport scheduler
     }
+
     //What i'm trying to achieve
     //Tone.Transport.schedule(function(){
     //  sampler.triggerAttack(samplerNote);
@@ -46,16 +47,16 @@ export class SequencerComponent implements OnInit {
 
   //this gets the matrix value out of the name
   getSeqCellVal(cellId){
-    var r = document.getElementById(cellId).id.charAt(cellId.length-2);
-    var c = document.getElementById(cellId).id.charAt(cellId.length-1);
-    var toggle = sequencerToggle[r][c];
+    var r = document.getElementById(cellId).id.charAt(cellId.length-3);
+    var c = document.getElementById(cellId).id.substring(cellId.length-2,cellId.length);
+    var toggle = sequencerToggle[r][parseInt(c)];
 
     return parseInt(toggle);
   }
 
   setSeqCellVal(cellId){
-    var r = document.getElementById(cellId).id.charAt(cellId.length-2);
-    var c = document.getElementById(cellId).id.charAt(cellId.length-1);
+    var r = document.getElementById(cellId).id.charAt(cellId.length-3);
+    var c = document.getElementById(cellId).id.substring(cellId.length-2,cellId.length);
     var toggle = sequencerToggle[parseInt(r)][parseInt(c)];
     if(toggle == 0){
       sequencerToggle[parseInt(r)][parseInt(c)]=1;
