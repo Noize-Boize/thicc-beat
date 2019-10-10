8d86021c9950f5432adf90d588f8fd15c07807d7
Shaun <sasheikh@uncg.edu>
2019-10-09 21:11:52 -0400

    color toggles on pads now

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -20,31 +20,49 @@ export class SequencerComponent implements OnInit {
 
   ngOnInit() {
 
+
+
   }
 
   //(not added)pass in 1. sampler value(C4) 2. point on transport(0:0:0)
   //include in arguments toggleValue
   activate(cellId){
-
-
-    //if(toggleValue = 0){
-    //sequencerToggle[r][c]=1
-    //turn on engaged color
-    document.getElementById(cellId).style.backgroundColor = "yellow";
-    //schedule value on transport}
-    //else{
-    //that means toggleValue=1 so it's engaged
-    //so sequencerToggle[r][c]=0 now
-    //take the note out of the transport scheduler
-    //change color back
-    //document.getElementById(cellId).style.backgroundColor = "black";
-
-    //}
-
+    var toggleValue = this.getSeqCellVal(cellId);
+    if(toggleValue == 0){
+      document.getElementById(cellId).style.backgroundColor = "yellow";
+      this.setSeqCellVal(cellId);
+      //schedule value on transport
+    }
+    else{
+      document.getElementById(cellId).style.backgroundColor = "black";
+      this.setSeqCellVal(cellId);
+      //take the note out of the transport scheduler
+    }
     //What i'm trying to achieve
     //Tone.Transport.schedule(function(){
     //  sampler.triggerAttack(samplerNote);
     //}, passedInTimeValue);
+  }
+
+  //this gets the matrix value out of the name
+  getSeqCellVal(cellId){
+    var r = document.getElementById(cellId).id.charAt(cellId.length-2);
+    var c = document.getElementById(cellId).id.charAt(cellId.length-1);
+    var toggle = sequencerToggle[r][c];
+
+    return parseInt(toggle);
+  }
+
+  setSeqCellVal(cellId){
+    var r = document.getElementById(cellId).id.charAt(cellId.length-2);
+    var c = document.getElementById(cellId).id.charAt(cellId.length-1);
+    var toggle = sequencerToggle[parseInt(r)][parseInt(c)];
+    if(toggle == 0){
+      sequencerToggle[parseInt(r)][parseInt(c)]=1;
+    }
+    else{
+      sequencerToggle[parseInt(r)][parseInt(c)]=0;
+    }
 
   }
 
