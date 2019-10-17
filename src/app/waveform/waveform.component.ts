755733080357ec2701e7e508c307286d23985f96
Cameron Raissi <cameronraissi@Camerons-MacBook-Pro.local>
2019-10-17 18:14:13 -0400

    Updating personal branch to shaun's push to master

--- src/app/waveform/waveform.component.ts
+++ src/app/waveform/waveform.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import * as WaveSurfer from 'wavesurfer.js';
+import * as Tone from 'tone';
+
+
+var wavePlayer = new Tone.Player("./../../assets/audioSamples/NOPE.mp3").toMaster();
+var item = null;
+var loadedTrackPath = "";
+
+
+
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-waveform',
@@ -11,5 +24,55 @@ export class WaveformComponent implements OnInit {
 
   ngOnInit() {
   }
+<<<<<<< Updated upstream
+=======
+  initWavesurfer(){
+    var wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'violet',
+      progressColor: 'purple'
+    });
+
+  wavesurfer.load(loadedTrackPath);
+
+  wavesurfer.on('ready', function () {
+    wavesurfer.play();
+});
+  }
+
+  loadFile(file){
+    if (file != null){
+      console.log("here in playTrack: not null")
+      //var path = "localhost:4200/"+file[0];
+      var path = window.URL.createObjectURL(file[0]);
+      loadedTrackPath = path;
+      wavePlayer.load(path);
+      //wavePlayer.load("./../../assets/audioSamples/HEY WHAT HAPPENED.mp3")
+      //console.log(file[0].webkitRelativePath.concat(file[0].name));
+      //console.log(path);
+      //console.log(path)
+      //console.log("webkitRelativePath: "+file[0].webkitRelativePath)
+
+    }
+    else{
+      console.log("here in playTrack: null")
+    }
+  }
+
+  playTrack(){
+    if(loadedTrackPath == "")
+    {
+      console.log("Track is default")
+      console.log(loadedTrackPath);
+      wavePlayer.start();
+    }
+    else{
+      wavePlayer.load(loadedTrackPath);
+      wavePlayer.start();
+    }
+  }
+
+
+>>>>>>> Stashed changes
 
 }
