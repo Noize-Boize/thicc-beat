d16458e17fc513b83055827f275ab93ab8c53b54
Jacob Butler <jak@Jacobs-MacBook-Pro.local>
2019-10-30 00:04:06 -0400

    Now with 3 less sequencer objects!

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -2,24 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import * as Tone from 'tone';
 
-
 Tone.Transport.start();
 
 // 1D array with 16 columnss
-var sequencerMatrix = new Array(16);
+var sequencerMatrix = new Array(4);
 
-var c4Array = new Array(16);
-var d4Array = new Array(16);
-var e4Array = new Array(16);
-var f4Array = new Array(16);
-
-// loop over 1D array to create 16x4 2D Array
-for(var i=0;i<16;i++){
-  sequencerMatrix[i] = [false,false,false,false];
-  c4Array[i] = '';
-  d4Array[i] = '';
-  e4Array[i] = '';
-  f4Array[i] = '';
+for(var i=0;i<4;i++){
+  sequencerMatrix[i] = ['','','','','','','','','','','','','','','',''];
 }
 
 var sampler = new Tone.Sampler({
@@ -31,53 +20,16 @@ var sampler = new Tone.Sampler({
   "baseUrl": "./../../assets/JakeAudioSample/"
 }).toMaster();
 
-var synth1 = new Tone.Synth().toMaster();
-
-var synth2 = new Tone.Synth().toMaster();
-
-var seq1 = new Tone.Sequence(function(time, note)
+var seq = new Tone.Sequence(function(time, col)
 {
-
-  if(!note){
+  for(var i = 0;i<4;i++)
+  {
+    if(sequencerMatrix[i][col] != '')
+    {
+      sampler.triggerAttack(sequencerMatrix[i][col]);
+    }
   }
-  else{
-    sampler.triggerAttack(note);
-
-  }
-
-}, c4Array, "16n");
-
-var seq2 = new Tone.Sequence(function(time, note)
-{
-
-  if(!note){
-  }
-  else{
-    sampler.triggerAttack(note);
-  }
-}, d4Array, "16n");
-
-var seq3 = new Tone.Sequence(function(time, note)
-{
-
-  if(!note){
-  }
-  else{
-    sampler.triggerAttack(note);
-  }
-}, e4Array, "16n");
-
-var seq4 = new Tone.Sequence(function(time, note)
-{
-
-  if(!note){
-  }
-  else{
-    sampler.triggerAttack(note);
-  }
-}, f4Array, "16n");
-
-
+},[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
 
 
 @Component({
@@ -85,123 +37,53 @@ var seq4 = new Tone.Sequence(function(time, note)
   templateUrl: './sequencer.component.html',
   styleUrls: ['./sequencer.component.css']
 })
+
+
 export class SequencerComponent implements OnInit {
 
+  ngOnInit()
+  {
+
+  }
   public notes: Array<string>;
   public columns: Array<string>;
-  constructor() {
 
+  constructor()
+  {
     this.notes = ['C4','D4','E4','F4'];
 
     this.columns = ['00','01','02','03','04','05','06','07',
                     '08','09','10','11','12','13','14','15'];
-
-   }
-
-  ngOnInit() {
   }
 
   play(){
-    if(seq1.state=="stopped"){
-      seq1.start();
-      seq2.start();
-      seq3.start();
-      seq4.start();
+    if(seq.state=="stopped"){
+      seq.start();
+      console.log('play started');
     }
     else
     {
-      seq1.stop();
-      seq2.stop();
-      seq3.stop();
-      seq4.stop();
+      seq.stop();
+      console.log('play stopped');
     }
     return;
   }
 
-
   toggleCell(event)
   {
+
     var id = event.target.attributes.id.textContent;
+    var cellValue = sequencerMatrix[event.target.dataset.y][event.target.dataset.x];
 
-    if(id.length == 7)
-    {
-      var x = id.slice(0,2);
-
-      var y = id.slice(3,4);
-
-      var note = id.slice(5);
-    }
-    else
-    {
-      var x = id.slice(0,1);
-
-      var y = id.slice(2,3);
-
-      var note = id.slice(4);
-    }
-    var cellValue = sequencerMatrix[x][y];
-
-    if(cellValue == false)
+    if(cellValue == '')
     {
       document.getElementById(id).style.backgroundColor = "#faed27";
-      this.changeArrayValue(note,x,y,false);
-
+      sequencerMatrix[event.target.dataset.y][event.target.dataset.x] = event.target.dataset.note;
     }
     else
     {
       document.getElementById(id).style.backgroundColor = "black";
-      this.changeArrayValue(note,x,y,true);
-
-    }
-    sequencerMatrix[x][y] = !sequencerMatrix[x][y];
-  }
-
-  changeArrayValue(note,x,y,boolean){
-
-    switch(note){
-      case 'C4':
-        if(boolean == true)
-        {
-          seq1.remove(x);
-        }
-        else
-        {
-            seq1.add(x,note);
-        }
-        break;
-      case 'D4':
-        if(boolean == true)
-        {
-          seq2.remove(x);
-        }
-        else
-        {
-            seq2.add(x,note);
-        }
-        break;
-      case 'E4':
-        if(boolean == true)
-        {
-          seq3.remove(x);
-        }
-        else
-        {
-            seq3.add(x,note);
-        }
-        break;
-      case 'F4':
-        if(boolean == true)
-        {
-          seq4.remove(x);
-        }
-        else
-        {
-            seq4.add(x,note);
-        }
-        break;
-      default:
-        break;
+      sequencerMatrix[event.target.dataset.y][event.target.dataset.x] = ''
     }
   }
-
 }
