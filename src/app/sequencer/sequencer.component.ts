6fec117c517eadc9fa013534dded30cb81cc6db8
Cameron Raissi <cameronraissi@wireless-152-13-52-111.uncg.edu>
2019-11-20 10:32:47 -0500

    play() function is basically implemented oninit. Made stepseq play pause button more "robust."

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -45,14 +45,35 @@ export class SequencerComponent implements OnInit {
 
 
   ngOnInit(){
+    //Toggle button to play/pause sequencer
+    var playButton = new Nexus.Button("#playButton", {'mode': 'toggle', 'state': false});
+    playButton.colorize("accent","#ff0000");
+    playButton.colorize("fill","#000");
+
+    playButton.on('change', function(v) {
+      if (seq.state=="stopped") {
+        seq.start();
+        console.log('play started');
+      }
+      else
+      {
+        seq.stop();
+        console.log('play stopped');
+      }
+    });
+
+
     //Slider for BPM init
     var BpmSlider = new Nexus.Slider("#BpmSlider", {'min': 60, 'max': 240, 'step': 1});
     BpmSlider.colorize("accent","#000");
     BpmSlider.colorize("fill","#ff0");
     BpmSlider.value = 90;
+    //assign slider to transport bpm
     BpmSlider.on('change', function(v) {Tone.Transport.bpm.value = v;});
     //text output of the current bpm inputted on slider
     var BpmNumDisplay= new Nexus.Number("#BpmNumDisplay");
+    BpmNumDisplay.colorize("accent","#000");
+    BpmNumDisplay.colorize("fill","#ff0");
     BpmNumDisplay.link(BpmSlider);
 
   }
@@ -66,7 +87,7 @@ export class SequencerComponent implements OnInit {
     this.columns = ['00','01','02','03','04','05','06','07',
                     '08','09','10','11','12','13','14','15'];
   }
-
+  /**   //play pause functionality implemented in nexusui oninit 
   play(){
     if(seq.state=="stopped"){
       seq.start();
@@ -79,6 +100,7 @@ export class SequencerComponent implements OnInit {
     }
     return;
   }
+  */
 
   toggleCell(event)
   {
