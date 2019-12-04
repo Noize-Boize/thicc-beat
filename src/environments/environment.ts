d12f7ee15edb06a303ac97f96c717b8be7630377
Jacob Butler <jak@Jacobs-MacBook-Pro.local>
2019-12-04 18:34:29 -0500

    Move to Group FIREBASE app. set up on console.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDcR65BOVrdP6zWazCwDpEkX-pEJp-sti4",
-    authDomain: "oncemorewithfeeling.firebaseapp.com",
-    databaseURL: "https://oncemorewithfeeling.firebaseio.com",
-    projectId: "oncemorewithfeeling",
-    storageBucket: "oncemorewithfeeling.appspot.com",
-    messagingSenderId: "44395608587",
-    appId: "1:44395608587:web:c741129953aec5836fa50e",
-    measurementId: "G-1V28QWR127"
+    apiKey: "AIzaSyC50t_TKDuS8hcjTojpvv4Q5wA1SwGaxTI",
+    authDomain: "thicc-beat-b7547.firebaseapp.com",
+    databaseURL: "https://thicc-beat-b7547.firebaseio.com",
+    projectId: "thicc-beat-b7547",
+    storageBucket: "thicc-beat-b7547.appspot.com",
+    messagingSenderId: "836260905549",
+    appId: "1:836260905549:web:516a5c7bec94d727fc1204",
+    measurementId: "G-GMPVGYWY4E"
+
     }
 
 };
